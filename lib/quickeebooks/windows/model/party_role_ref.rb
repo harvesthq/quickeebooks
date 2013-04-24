f366082ce2a8731c4cf09cfc89e8f512035b2ff1
Ryan Bright <ryan@rbright.net>
2013-04-23 23:05:12 -0400

    Add SyncToken as an attribute for PartyRoleRef

--- lib/quickeebooks/windows/model/party_role_ref.rb
+++ lib/quickeebooks/windows/model/party_role_ref.rb
@@ -4,6 +4,7 @@ module Quickeebooks
       class PartyRoleRef < Quickeebooks::Windows::Model::IntuitType
         xml_convention :camelcase
         xml_accessor :id, :as => Quickeebooks::Windows::Model::Id
+        xml_accessor :sync_token, :from => 'SyncToken', :as => Integer
         xml_accessor :party_reference_id, :as => Quickeebooks::Windows::Model::Id
         xml_accessor :last_updated_time, :as => Time
       end
